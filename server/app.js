bbcb71b546e1c675589ea964df8b0f6a3d911d96
nickem00 <nickemalm00@gmail.com>
2025-04-21 17:21:11 +0200

    added connection to database and server in app.js. Also added files for models

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 import app from "./src/express.js";
-import 'dotenv/config';
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 5000;
+// Load environment variables from .env file
+dotenv.config({ path: "../.env"});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+
+// Connect to MongoDB and start the server
+mongoose.connect(MONGODB_CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB", err);
+    });
